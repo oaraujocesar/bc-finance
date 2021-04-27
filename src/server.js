ebf4f8f3a894321879e0fba4bdc69b9db53b1eb3
César O. Araújo <oaraujocesar@gmail.com>
2021-04-26 23:07:21 -0300

    feat: :card_file_box: connecting to mongo

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import cors from 'cors'
 
 require('dotenv').config()
@@ -8,6 +9,8 @@ import routes from './routes'
 const server = express()
 
 const port = 3333 || process.env.PORT
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wimfi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
 
 server.use(cors())
 server.use(express.json())

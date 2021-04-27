30d6c1d1b3a8812d70858ab748dd5024121a2484
César O. Araújo <oaraujocesar@gmail.com>
2021-04-27 19:23:53 -0300

    refactor: :fire: removing unused variable

--- src/app/jobs/fiis.js
+++ src/app/jobs/fiis.js
@@ -2,7 +2,7 @@ import cron from 'node-cron'
 import fetch from 'node-fetch'
 
 const cronFii = () => cron.schedule('0 1 * * *', async () => { 
-  const res = await fetch(`http://localhost:${process.env.PORT}/api/v1/fiis`)
+  await fetch(`http://localhost:${process.env.PORT}/api/v1/fiis`)
 }, { scheduled: true, timezone: "America/Sao_Paulo"})
 
 export default cronFii
